1db1c4291253bf65eb60a2d316dad52f706013f7
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-03-03 09:39:39 +0700

    Added constructor on  user service with parameter for iject repository user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // Constructor with parameter inject repository user
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository,
+  ) {}
+}
