4b29c0cbf57bc92cfdf5b514fe94a56bb923bbfb
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-03-03 11:23:32 +0700

    Moved folder config into src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from 'config/database.config';
+import { typeOrmConfig } from 'src/config/database.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
