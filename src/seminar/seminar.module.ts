a9c45728526045468f01a46c5faa3fd754d0d998
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-03-03 15:25:07 +0700

    Create seminar repository and service

--- src/seminar/seminar.module.ts
+++ src/seminar/seminar.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
+import { SeminarService } from './seminar.service';
 
 @Module({
   imports: [
+    //   Import seminar
+    TypeOrmModule.forFeature([SeminarModule]),
     //   Import user module
     UsersModule,
   ],
+  providers: [SeminarService],
 })
 export class SeminarModule {}
---
+++ src/seminar/seminar.repository.ts
@@ -0,0 +1,5 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Seminar } from './entity/seminar.entity';
+
+@EntityRepository(Seminar)
+export class SeminarRepository extends Repository<Seminar> {}
