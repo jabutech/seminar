a672d63929dc816ba69bd27b3447642da5be47a9
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-03-03 09:06:27 +0700

    Install bcrypt

---
+++ src/users/entity/user.entity.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  token: string;
+
+  @BeforeInsert
+  @BeforeUpdate
+}
