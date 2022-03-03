e4ad35516dacebc62c801e30cafb7da82e225e10
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-03-03 09:42:20 +0700

    Added method register on user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -9,4 +10,9 @@ export class UsersService {
     @InjectRepository(UserRepository)
     private readonly userRepository: UserRepository,
   ) {}
+
+  //   Method service register
+  async register(payloadUser: CreateUserDto): Promise<void> {
+    return await this.userRepository.register(payloadUser);
+  }
 }
