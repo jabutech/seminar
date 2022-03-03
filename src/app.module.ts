c187f44dcbb85147ff659a96b117108dad3b2618
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-03-03 14:59:20 +0700

    Added module seminar

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from 'src/config/database.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { SeminarModule } from './seminar/seminar.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
     AuthModule,
+    SeminarModule,
   ],
 })
 export class AppModule {}
